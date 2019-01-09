85933472fb730cbf90fb57efb61d59bdfc53f273
Sebastian Poblete <sebapobletec@gmail.com>
2019-01-09 19:01:00 -0300

    Ejercicio2

--- Ejercicio2.rb
+++ Ejercicio2.rb
@@ -10,7 +10,7 @@ end
 
 class Rectangulo
   include Formula
-  attr_accessor :base 
+  attr_accessor :base
   attr_accessor :altura
   def initialize(base, altura)
     @base = base
@@ -24,6 +24,7 @@ end
 
 class Cuadrado
   include Formula
+  attr_accessor :lado
   def initialize(lado)
     @lado = lado
   end
@@ -35,5 +36,10 @@ end
 
 rectangulo1 = Rectangulo.new(rand(1..10), rand(1..10))
 cuadrado1 = Cuadrado.new(rand(1..10))
-puts rectangulo1.
 puts cuadrado1.lados
+puts cuadrado1.area(cuadrado1.lado, cuadrado1.lado)
+puts cuadrado1.perimetro(cuadrado1.lado, cuadrado1.lado)
+
+puts rectangulo1.lados
+puts rectangulo1.area(rectangulo1.base, rectangulo1.altura)
+puts rectangulo1.perimetro(rectangulo1.base, rectangulo1.altura)
